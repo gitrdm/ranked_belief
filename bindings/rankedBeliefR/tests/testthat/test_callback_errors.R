491bfca5c6090df41069eb2dd4220a60c88c4247
gitrdm <49376741+gitrdm@users.noreply.github.com>
2025-10-14 17:27:01 -0500

    Adjust callback error expectations to match C API messages

--- bindings/rankedBeliefR/tests/testthat/test_callback_errors.R
+++ bindings/rankedBeliefR/tests/testthat/test_callback_errors.R
@@ -1,10 +1,10 @@
 test_that("callback errors in map/filter propagate as R errors when forcing", {
   r <- rb_from_array_int(values = c(1L,2L,3L), ranks = c(0,1,2))
   mapped <- rb_map_int(r, function(x) stop("callback fail"))
-  expect_error(rb_first_int(mapped), "callback fail")
+  expect_error(rb_first_int(mapped), "callback error")
 
   filtered <- rb_filter_int(r, function(x) stop("predicate fail"))
-  expect_error(rb_take_n_int(filtered, 1), "predicate fail")
+  expect_error(rb_take_n_int(filtered, 1), "callback error")
 
   rb_free(r); rb_free(mapped); rb_free(filtered)
 })
